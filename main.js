68716cfb0fe864777797780e74f6fd39c9bd2ca3
aahana924 <87435688+aahana924@users.noreply.github.com>
2022-02-20 08:11:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+var images = ["capture.PNG","file.jpeg"];
+var names = ["Mama Aruna Gupta","Papa Abhi Singh"];
+
+var i = 0;
+function update()
+{
+    i++;
+    var numbers_of_family_member_in_array = 1
+    if(i > numbers_of_family_member_in_array)
+      {
+          i = 0;
+      }
+    
+    //Debug the code to store list of images in updatedImage. Use images[i]
+    var updatedImage = images[i];
+    //Debug the code to store list of names in updatedName. Use names[i]
+    var updatedName = names[i] ;
+ 
+    document.getElementById("family_member_image").src = updatedImage;
+    document.getElementById("family_member_name").innerHTML = updatedName;
+}
